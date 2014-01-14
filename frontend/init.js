ec56355e9a5132dbe38ce78a75de04c759754f56
bla <bla@fuckyoudontspammyemailovereverything.com>
2014-01-14 17:46:21 -0600

    made menu.js be loaded by require.js, hopefully this fixes the null parent issue

--- frontend/init.js
+++ frontend/init.js
@@ -1,5 +1,5 @@
-require(['brushes/brushes', 'canvas'],
-	function(brushes, canvas)
+require(['menu', 'brushes/brushes', 'canvas'],
+	function(menu, brushes, canvas)
 	{
 		canvas_init = canvas_script();
 		canvas_init('canvas', window.innerWidth - 17, window.innerHeight - 45, [0,0,0,1.0], 10);
