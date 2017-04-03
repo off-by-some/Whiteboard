232f26a02c2bb5efdf9963aa06b0b85abb641a75
Pholey <cassidybridges@gmail.com>
2017-04-02 19:40:18 -0700

    Add awesome example for great good

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # Whiteboard ala WebGL
+
+
+Example WebGL API:
+```
+<WebGLCanvas>
+    <WebGLCircle
+      x={500}
+      y={500}
+      radius={200}
+      color={faceColor}
+    />
+    <WebGLCircle
+      x={500}
+      y={500}
+      radius={100}
+      color={[Math.random(), Math.random(), Math.random(), 1]}
+    />
+    <WebGLCircle
+      x={500}
+      y={500}
+      radius={90}
+      color={faceColor}
+    />
+
+    <WebGLRect
+      x={400}
+      y={400}
+      height={100}
+      width={200}
+      radius={90}
+      color={faceColor}
+    />
+
+    <WebGLCircle
+      x={450}
+      y={450}
+      radius={20}
+      color={[Math.random(), Math.random(), Math.random(), 1]}
+    />
+    <WebGLCircle
+      x={550}
+      y={450}
+      radius={20}
+      color={[Math.random(), Math.random(), Math.random(), 1]}
+    />
+</WebGLCanvas>
+```
--- src/App.js
+++ src/App.js
@@ -8,44 +8,48 @@ import WebGLCircle from "./components/WebGLCircle";
 @Autobind
 class App extends Component {
   render() {
+    const faceColor = [1, 0.85, 0, 1]
+
     return (
        <WebGLCanvas>
-         <WebGLRect
-           x={0}
-           y={0}
-           width={200}
-           height={200}
-           color={[Math.random(), Math.random(), Math.random(), 1]}
-          />
-
-          <WebGLRect
-            x={100}
-            y={100}
-            width={200}
-            height={200}
-            color={[Math.random(), Math.random(), Math.random(), 1]}
+           <WebGLCircle
+             x={500}
+             y={500}
+             radius={200}
+             color={faceColor}
+           />
+           <WebGLCircle
+             x={500}
+             y={500}
+             radius={100}
+             color={[Math.random(), Math.random(), Math.random(), 1]}
+           />
+           <WebGLCircle
+             x={500}
+             y={500}
+             radius={90}
+             color={faceColor}
            />
 
            <WebGLRect
-             x={200}
-             y={200}
+             x={400}
+             y={400}
+             height={100}
              width={200}
-             height={200}
-             color={[Math.random(), Math.random(), Math.random(), 1]}
-            />
-
-          <WebGLRect
-            x={400}
-            y={400}
-            width={200}
-            height={200}
-            color={[Math.random(), Math.random(), Math.random(), 1]}
+             radius={90}
+             color={faceColor}
            />
 
            <WebGLCircle
              x={450}
              y={450}
-             radius={200}
+             radius={20}
+             color={[Math.random(), Math.random(), Math.random(), 1]}
+           />
+           <WebGLCircle
+             x={550}
+             y={450}
+             radius={20}
              color={[Math.random(), Math.random(), Math.random(), 1]}
            />
       </WebGLCanvas>
